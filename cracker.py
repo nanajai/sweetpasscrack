a42d4c9b11355402aab26b1e13da77d66a81c650
emtseng <etseng42@gmail.com>
2017-11-02 22:11:36 -0400

    adds structure derivation and test case for no match to rockyou top 100

--- cracker.py
+++ cracker.py
@@ -1,12 +1,28 @@
 import pandas as pd
 import numpy as np
+import sys
+import csv
 import codecs
 import string
-import csv
-import sys
+from random import randint
+
+def getPart(ch):
+    if (ch.isalpha()):
+        return "L"
+    elif (ch.isdigit()):
+        return "D"
+    else: return "S"
 
 
-def get_all_passwords():
+def deriveStructure(password):
+    parts = []
+    for idx in range(0, len(password)):
+        parts.append(getPart(password[idx]))
+
+    return str("".join(parts))
+
+
+def get_100_passwords():
     dataset = codecs.open('rockyou_password.txt', 'r', encoding='utf-8', errors='ignore')
     entries = dataset.read().split("\n")
 
@@ -25,14 +41,23 @@ def get_all_passwords():
     return passwords
 
 
+# Returns hashmap mapping structures to their frequencies within the top 100 most common passwords of the RockYou DB.
+def get_struct_map(passList):
+    freqMap = {}
+    for password in passList:
+        struct = deriveStructure(password)
+        if struct in freqMap:
+            freqMap[struct] += 1
+        else:
+            freqMap[struct] = 1
+    return freqMap
+
+
 #Returns a hashmap of the ranks of passwords within the top 100. It will look for the password within a sweetword
 def get_rockyou_pass(sweetwords_list):
-
-    passwords = get_all_passwords()
+    passwords = get_100_passwords()
     pass_dict = dict.fromkeys(sweetwords_list, 101)
-    print(pass_dict)
     for sweetword in sweetwords_list:
-        print("Sweetword {}",sweetword)
         for i,password in enumerate(passwords):
             if(password in sweetword):
                 print(password)
@@ -43,14 +68,14 @@ def get_rockyou_pass(sweetwords_list):
 
 
 
-
-
 def guess_password(sweetwords):
-    print("This is sweetwords {}",sweetwords)
+    print("Sweetwords set: ", sweetwords)
     sweetwords_list = sweetwords.split(',')
     pass_dict = get_rockyou_pass(sweetwords_list)
-
-    print(pass_dict)
+    print("pass_dict: ", pass_dict)
+    
+    struct_map = get_struct_map(get_100_passwords())
+    print("struct_map: ", struct_map)
 
     highestRank = sorted(pass_dict.values())[0]
     if(highestRank != 101):
@@ -59,10 +84,29 @@ def guess_password(sweetwords):
                     print(key)
                     return
     else: #look at another option
-        return 'no password' #for now
-
-
-
+        print('No match to RockYou top 100 found. Searching structurally.')
+        sweetwords_struct_map = {}
+        
+        # Create list of structures
+        sweetword_structs = {}
+        for sweetword in pass_dict.keys():
+            struct = deriveStructure(sweetword)
+            freq = 0
+            if struct in struct_map:
+                freq = struct_map[struct]
+            ## TODO: What if the structure of the sweetword isn't in the struct_map?
+            sweetword_structs[sweetword] = freq
+        
+        # Get the sweetwords with the most freq structures in a list.
+        # Output one of them at random -- they are flat.
+        print(sweetword_structs)
+        topFreq = sorted(sweetword_structs.values())[-1]
+        mostFreq = []
+        for sWord,freq in sweetword_structs.items():
+            if freq == topFreq:
+                mostFreq.append(sWord)
+        print(mostFreq[randint(0, len(mostFreq)-1)])
+        return
 
 
 def main():
@@ -74,7 +118,7 @@ def main():
         for line in i:
             if line == '' or line == '\n': break
             newline = line.rstrip()
-            sweetwords = guess_password(newline)
+            guess_password(newline)
     i.close()
 
 
